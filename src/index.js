88137f5be30183cfaa443c7f6f9f938e811650ce
Joel-100 <joelkugonza@gmail.com>
2022-09-30 18:28:57 +0300

    implimented edit function

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const form = document.querySelector('#addForm');
 
 // Declaring the array to hold the various tasks
 const tasks = JSON.parse(localStorage.getItem('data')) || [];
-
 // Add toDo to the local storage
 const addToDo = () => {
   form.addEventListener('submit', (e) => {
@@ -37,9 +36,10 @@ const displayToDos = () => {
   task += `<div class="list">
   <div class="input">
     <input class="tsk" type="checkbox" />
-    <input class="tsk" type="text" value="${item.description}" />
+    <input class="tsk list-input" type="text" value="${item.description}" />
   </div>
-  <i class="fa-solid fa-ellipsis-vertical"></i>
+  <i class="fa-solid fa-ellipsis-vertical dots"></i>
+  <i class="fa-solid fa-trash delete"></i>
 </div>
 `;
 // Inserting the items in the activities section
@@ -47,5 +47,26 @@ activities.innerHTML = task;
   });
 };
 
+// Edit an item in the array 
+const edit = () => {
+// Grabbing all the input fields in the various items  
+  const listInputs = document.querySelectorAll('.list-input');
+  listInputs.forEach((item, id) => {
+    // Traverse the DOM to get the parent of the item
+    item.addEventListener('focus', () => {
+      item.parentElement.parentElement.style.background = 'rgb(255, 255, 193)';
+      item.parentElement.nextElementSibling.style.display = 'none';
+      item.parentElement.parentElement.lastElementChild.style.display = 'block';
+    });
+
+    item.addEventListener('blur', () => {
+      item.parentElement.parentElement.style.background = 'white';
+      item.parentElement.nextElementSibling.style.display = 'block';
+      item.parentElement.parentElement.lastElementChild.style.display = 'none';
+    });
+  }); 
+};
+
 addToDo();
 displayToDos();
+edit();
